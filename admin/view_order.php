f1c9aad2c08a4af118317563d17e566abb9f3e50
nuraddinaizzah <113405384+nuraddinaizzah@users.noreply.github.com>
2024-07-01 23:01:22 +0800

    Add files via upload

---
+++ admin/view_order.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("../connection/connect.php");
+error_reporting(0);
+session_start();
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="main logo.png">
+    <title>Hardware - Admin Dashboard</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/helper.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:** -->
+    <!--[if lt IE 9]>
+    <script src="https:**oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https:**oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<script language="javascript" type="text/javascript">
+var popUpWin=0;
+function popUpWindow(URLStr, left, top, width, height)
+{
+ if(popUpWin)
+{
+if(!popUpWin.closed) popUpWin.close();
+}
+popUpWin = open(URLStr,'popUpWin', 'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=no,copyhistory=yes,width='+600+',height='+600+',left='+left+', top='+top+',screenX='+left+',screenY='+top+'');
+}
+
+</script>
+</head>
+
+<body class="fix-header fix-sidebar">
+    <!-- Preloader - style you can find in spinners.css -->
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+			<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+    </div>
+    <!-- Main wrapper  -->
+    <div id="main-wrapper">
+        <!-- header header  -->
+        <div class="header">
+            <nav class="navbar top-navbar navbar-expand-md navbar-light">
+                <!-- Logo -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="dashboard.php">
+                          <!-- Logo icon -->
+                          <b><img src="main logo.png" alt="homepage" class="dark-logo" width = "65px"/></b>
+                        <!--End Logo icon -->
+                        <!-- Logo text -->
+                        <span><b>HARDWARE</b></span>
+                    </a>
+                </div>
+                <!-- End Logo -->
+                <div class="navbar-collapse">
+                    <!-- toggle and nav items -->
+                    <ul class="navbar-nav mr-auto mt-md-0">
+                        <!-- This is  -->
+                        <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted  " href="javascript:void(0)"><i class="mdi mdi-menu"></i></a> </li>
+                        <li class="nav-item m-l-10"> <a class="nav-link sidebartoggler hidden-sm-down text-muted  " href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
+                     
+                       
+                    </ul>
+                    <!-- User profile and search -->
+                    <ul class="navbar-nav my-lg-0">
+
+                       
+                        <!-- Comment -->
+                        <li class="nav-item dropdown">
+                           
+                            <div class="dropdown-menu dropdown-menu-right mailbox animated zoomIn">
+                                <ul>
+                                    <li>
+                                        <div class="drop-title">Notifications</div>
+                                    </li>
+                                    
+                                    <li>
+                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <!-- End Comment -->
+                      
+                        <!-- Profile -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-muted  " href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="images/users/5.jpg" alt="user" class="profile-pic" /></a>
+                            <div class="dropdown-menu dropdown-menu-right animated zoomIn">
+                                <ul class="dropdown-user">
+                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> LogOut</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <!-- End header header -->
+        <!-- Left Sidebar  -->
+        <div class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                   <ul id="sidebarnav">
+                        <li class="nav-devider"></li>
+                        <li class="nav-label">Admin Panel</li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Home</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="dashboard.php">Dashboard</a></li>
+                                
+                            </ul>
+                        </li>
+                        
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false">  <span><i class="fa fa-user f-s-20 "></i></span><span class="hide-menu">Users</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="allusers.php">All Users</a></li>
+							
+								
+                               
+                            </ul>
+                        </li>
+                        <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-archive f-s-20 color-warning"></i><span class="hide-menu">About Shop</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="allstores.php">View profile</a></li>
+								  
+                            </ul>
+                        </li>
+                      <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-desktop" aria-hidden="true"></i><span class="hide-menu">Product</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="all_product.php">All Product</a></li>
+								<li><a href="add_product.php">Add Product</a></li>
+                              
+                                
+                            </ul>
+                        </li>
+						 <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span class="hide-menu">Orders</span></a>
+                            <ul aria-expanded="false" class="collapse">
+								<li><a href="all_orders.php">All Orders</a></li>
+								  
+                            </ul>
+                        </li>
+                         
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </div>
+        <!-- End Left Sidebar  -->
+        <!-- Page wrapper  -->
+        <div class="page-wrapper">
+            <!-- Bread crumb -->
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-primary">View Users Orders</h3> </div>
+                
+            </div>
+            <!-- End Bread crumb -->
+            <!-- Container fluid  -->
+            <div class="container-fluid">
+                <!-- Start Page Content -->
+                <div class="row">
+                    <div class="col-12">
+                        
+                       
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">All User Orders List</h4>
+                             
+                                <div class="table-responsive m-t-20">
+                                    <table id="myTable" class="table table-bordered table-striped">
+                                       
+                                        <tbody>
+                                           <?php
+											$sql="SELECT users.*, users_orders.* FROM users INNER JOIN users_orders ON users.u_id=users_orders.u_id where o_id='".$_GET['user_upd']."'";
+												$query=mysqli_query($db,$sql);
+												$rows=mysqli_fetch_array($query);
+												
+												
+																		
+												?>
+											
+											<tr>
+													<td><strong>Username:</strong></td>
+												    <td><center><?php echo $rows['username']; ?></center></td>
+													   <td>
+    <?php
+$status = $rows['status'];
+
+// Check if the status is 'delivered', 'cancelled', or 'rejected'
+if ($status != 'closed' && $status != 'cancelled' && $status != 'rejected') {
+    ?>
+    <center>
+        <a href="javascript:void(0);" onClick="popUpWindow('order_update.php?form_id=<?php echo htmlentities($rows['o_id']);?>');" title="Update order">
+            <button type="button" class="btn btn-primary">Take Action</button>
+        </a>
+    </center>
+<?php
+} else {
+    // If status is 'delivered', 'cancelled', or 'rejected', disable the button and remove hover effect
+    ?>
+    <style>
+        .disabled-button {
+            pointer-events: none;
+        }
+    </style>
+    <center>
+        <button type="button" class="btn btn-secondary disabled-button" disabled>Take Action</button>
+    </center>
+<?php
+}
+?>
+
+</td>
+
+												  
+																																					
+											</tr>	
+											<tr>
+												<td><strong>Ordered Products:</strong></td>
+												    <td><center><?php echo $rows['title']; ?></center></td>
+													    <td><center>
+													   <a href="javascript:void(0);" onClick="popUpWindow('userprofile.php?newform_id=<?php echo htmlentities($rows['o_id']);?>');" title="Update order">
+														<button type="button" class="btn btn-primary">View User Details</button></a>
+											
+													   </center></td>
+												   																								
+											</tr>	
+											<tr>
+													<td><strong>Quantity:</strong></td>
+												    <td><center><?php echo $rows['quantity']; ?></center></td>
+													  
+												   																							
+											</tr>
+											<tr>
+													<td><strong>Price:</strong></td>
+												    <td><center>RM <?php echo $rows['price']; ?></center></td>
+													   
+												   																							
+											</tr>
+											<tr>
+													<td><strong>Address:</strong></td>
+												    <td><center><?php echo $rows['address']; ?></center></td>
+													  
+												   																							
+											</tr>
+											<tr>
+													<td><strong>Date:</strong></td>
+												     <td><center><?php echo $rows['date']; ?></center></td>
+													  
+												   																							
+											</tr>
+											<tr>
+													<td><strong>Status:</strong></td>
+													
+																			<?php
+$status = $rows['status'];
+if ($status == "" or $status == "NULL") {
+?>
+    <td> <center><button type="button" class="btn btn-info" style="font-weight:bold;"><span class="fa fa-bars" aria-hidden="true">Customer place order</button></center></td>
+<?php
+} elseif ($status == "in process") { ?>
+    <td> <center><button type="button" class="btn btn-warning"><span class="fa fa-cog fa-spin" aria-hidden="true"></span>In process</button></center></td>
+<?php
+} elseif ($status == "closed") { ?>
+    <td><center> <button type="button" class="btn btn-success"><span class="fa fa-check-circle" aria-hidden="true">Delivered</button></center></td>
+<?php
+} elseif ($status == "rejected") { ?>
+    <td> <center><button type="button" class="btn btn-danger"><i class="fa fa-close"></i>Cancelled </button></center></td>
+<?php
+} elseif ($status == "cancelled") { ?>
+    <td> <center> <button type="button" class="btn btn-danger"><i class="fa fa-close"></i>Cancelled by customer</button></center></td>
+<?php
+}
+?>
+
+																			
+													  
+												   																							
+											</tr>
+											
+																				
+																															
+																						
+									
+                                            
+                                           
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+						 </div>
+                      
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End PAge Content -->
+            </div>
+            <!-- End Container fluid  -->
+			
+			
+			
+			
+            <!-- footer -->
+            <footer class="footer"> © 2024 All rights reserved. Made  by <b><a> HARDWARE</a></b> Team. </footer>
+            <!-- End footer -->
+        </div>
+        <!-- End Page wrapper  -->
+    </div>
+    <!-- End Wrapper -->
+    <!-- All Jquery -->
+    <script src="js/lib/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="js/lib/bootstrap/js/popper.min.js"></script>
+    <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.min.js"></script>
+
+
+    <script src="js/lib/datatables/datatables.min.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
+    <script src="js/lib/datatables/cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script src="js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+    <script src="js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+    <script src="js/lib/datatables/datatables-init.js"></script>
+</body>
+
+</html>
